3012d96c856bbecbfc725e713888b0235b96cd17
unknown <paulwilliamfussell@gmail.com>
2018-11-20 03:18:35 -0800

    re-implementing refactor (my bad)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="app">
-    <!-- 
+    
     <Navbar />
       <router-view />
-    <Footer/>-->
+    <Footer/>
   </div> 
 
 </template>
 <script>
-// import Navbar from '@/components/Navbar.vue';
-// import Footer from '@/components/Footer.vue';
+import Navbar from '@/components/Navbar.vue';
+import Footer from '@/components/Footer.vue';
 
-// export default {
-//   components: {
-//     Navbar,
-//     Footer
-//   }
-// }
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
 </script>
 
 
 <style>
-/* #app {
+#app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -39,5 +39,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-} */
+}
 </style>
