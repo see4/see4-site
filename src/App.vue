19a8a9a27d810d400d2c5d9925048a56620c87ee
unknown <paulwilliamfussell@gmail.com>
2018-11-20 02:49:46 -0800

    new images

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,27 @@
 <template>
   <div id="app">
+    <!-- 
     <Navbar />
       <router-view />
-    <Footer/>
-  </div>
+    <Footer/>-->
+  </div> 
+
 </template>
 <script>
-import Navbar from '@/components/Navbar.vue';
-import Footer from '@/components/Footer.vue';
+// import Navbar from '@/components/Navbar.vue';
+// import Footer from '@/components/Footer.vue';
 
-export default {
-  components: {
-    Navbar,
-    Footer
-  }
-}
+// export default {
+//   components: {
+//     Navbar,
+//     Footer
+//   }
+// }
 </script>
 
 
 <style>
-#app {
+/* #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -32,10 +34,10 @@ export default {
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #56126b;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
