d2662034991f488bf03a58ba1c7d6036070dd32a
unknown <paulwilliamfussell@gmail.com>
2018-11-27 06:45:11 -0800

    temp header for Admin, Home, and Profile Page

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,6 +1,8 @@
 <template>
+
     <div>
-        <h1>Admin Page</h1>
+        <div class="temphead"></div>
+        <h1 class="temp">Admin Page</h1>
     </div>
 </template>
 <script>
@@ -9,7 +11,15 @@ export default {
 }
 </script>
 <style>
+.temp {
+  margin-top: 100px;
+}
 
+.temphead {
+  width: 100%;
+  height: 100px;
+  background-color: #212529;
+}
 </style>
 
 
