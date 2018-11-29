a0186354683a2d92b775052baf8aef6706d13341
francislagueu <lagson6@yahoo.fr>
2018-11-29 01:53:01 -0800

    Contact form submission completed

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -496,28 +496,28 @@ Our mission is simple; add convenience to the lives of our customer’s while pr
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
-                    <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name.">
+                    <input class="form-control" v-model="contactForm.name" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name.">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address.">
+                    <input class="form-control" v-model="contactForm.email" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address.">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-                    <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number.">
+                    <input class="form-control" v-model="contactForm.phone" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number.">
                     <p class="help-block text-danger"></p>
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="form-group">
-                    <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                    <textarea class="form-control" v-model="contactForm.message" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
                     <p class="help-block text-danger"></p>
                   </div>
                 </div>
                 <div class="clearfix"></div>
                 <div class="col-lg-12 text-center">
                   <div id="success"></div>
-                  <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+                  <button id="sendMessageButton" @click.prevent="sendMessage" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
                 </div>
               </div>
             </form>
@@ -737,8 +737,51 @@ Our mission is simple; add convenience to the lives of our customer’s while pr
 </template>
 
 <script>
+import firebase from 'firebase';
 export default {
   name: "welcome",
+  data(){
+    return {
+      contactForm: {
+        name: '',
+        email: '',
+        phone: '',
+        message: ''
+      }
+    }
+  },
+  methods: {
+    sendMessage(){
+      firebase.firestore().collection('messages').add({
+        name: this.contactForm.name,
+        email: this.contactForm.email,
+        phone: this.contactForm.phone,
+        message: this.contactForm.message,
+        createdAt: Date.now()
+      }).then((docRef)=>{
+        this.$swal(
+          'Thank you ',
+          'You message has been successfully submitted',
+          'success'
+        );
+      }).catch((error)=>{
+        console.log(error);
+        this.$swal(
+          'Error!!!',
+          'Something went wrong '+ error.message,
+          'error'
+        )
+      });
+      this.resetForm();
+    },
+
+    resetForm(){
+      this.contactForm.name = '';
+      this.contactForm.email = '';
+      this.contactForm.phone = '';
+      this.contactForm.message = '';
+    }
+  }
   
 };
 </script>
